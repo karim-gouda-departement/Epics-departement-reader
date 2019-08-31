bf3b229de858f58e33874ba906eae2b2a046ea0e
Karim Gouda Said Hessan <gouda.karim@hotmail.com>
2019-08-31 12:59:27 +0200

    Initial Commit

---
+++ Epics departement reader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Epics departement reader
+//
+//  Created by Karim Gouda Said Hessan on 31/08/2019.
+//  Copyright © 2019 Karim Gouda Said Hessan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
